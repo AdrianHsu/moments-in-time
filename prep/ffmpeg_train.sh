30a85f96b925c4b3f07042ce13537e9b83df699a
AdrianHsu <adrianhsu1995@gmail.com>
2018-05-05 23:20:40 +0800

    -c pcm_s16le -ac 1 -ar 16000

--- prep/ffmpeg_train.sh
+++ prep/ffmpeg_train.sh
@@ -1,21 +1,20 @@
 
 ROOT='/tmp3/Moments_in_Time_Mini'
-VAL_DAT=$ROOT/validation
 TRAIN_DAT=$ROOT/training
 
 echo $TRAIN_DAT
 
 for line in $(find $TRAIN_DAT); 
 do 
-    var=$(echo $line | sed "s;training;audio\/training;g")
+    var=$(echo $line | sed "s;training;audio_mono\/training;g")
     wav=$(echo $var | sed "s/mp4/wav/g")
     dir=$(echo $wav | cut -d '/' -f1-6)
     mkdir -p $dir
-    ffmpeg -i $line $wav
+    ffmpeg -i $line -c pcm_s16le -ac 1 -ar 16000 $wav
     if [ $? -ne "0" ]; then
-        echo 'FAIL,'$wav >> path_train.txt
+        echo 'FAIL,'$wav','$line >> /tmp3/Moments_in_Time_Mini/audio_mono/path_train.txt
     else
-        echo 'OK,'$wav >> path_train.txt
+        echo 'OK,'$wav','$line >> /tmp3/Moments_in_Time_Mini/audio_mono/path_train.txt
     fi
 
 done
--- prep/ffmpeg_val.sh
+++ prep/ffmpeg_val.sh
@@ -1,21 +1,20 @@
 
 ROOT='/tmp3/Moments_in_Time_Mini'
 VAL_DAT=$ROOT/validation
-TRAIN_DAT=$ROOT/training
 
 echo $VAL_DAT
 
 for line in $(find $VAL_DAT); 
 do 
-    var=$(echo $line | sed "s;validation;audio\/validation;g")
+    var=$(echo $line | sed "s;validation;audio_mono\/validation;g")
     wav=$(echo $var | sed "s/mp4/wav/g")
     dir=$(echo $wav | cut -d '/' -f1-6)
     mkdir -p $dir
-    ffmpeg -i $line $wav
+    ffmpeg -i $line -c pcm_s16le -ac 1 -ar 16000 $wav
     if [ $? -ne "0" ]; then
-        echo 'FAIL,'$wav >> path.txt
+        echo 'FAIL,'$wav','$line >> /tmp3/Moments_in_Time_Mini/audio_mono/path_val.txt
     else
-        echo 'OK,'$wav >> path.txt
+        echo 'OK,'$wav','$line >> /tmp3/Moments_in_Time_Mini/audio_mono/path_val.txt
     fi
 
 done
